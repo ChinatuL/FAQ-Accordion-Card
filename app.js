0b8dda08fa70f30502014e4fca586688dcf370bd
Chinatu Lucia Eke <chinatuluciaeke@gmail.com>
2023-02-18 16:23:42 +0100

    add html, css, js and image files

---
+++ app.js
@@ -0,0 +1,24 @@
+const questions = document.querySelectorAll(".question");
+
+questions.forEach((question) => {
+    const btn = question.querySelector(".btn");
+    const title = question.querySelector("h2");
+    question.addEventListener("click", () => {
+        questions.forEach((item) => {
+            const itemTitle = item.querySelector("h2");
+            if (item !== question) {
+                item.classList.remove("show-answer");
+                itemTitle.classList.remove("highlight-title");
+            }
+        });
+        question.classList.toggle("show-answer");
+        btn.classList.toggle("rotate-btn");
+        if (!question.classList.contains("show-answer")) {
+            question.setAttribute("aria-expaned", "false");
+            title.classList.remove("highlight-title");
+        } else {
+            question.setAttribute("aria-expanded", "true");
+            title.classList.add("highlight-title");
+        }
+    });
+});
